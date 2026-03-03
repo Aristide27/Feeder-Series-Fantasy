edef3a59b068e7c1204bee258db89c0aaa08dacf
Aristide <aristide.lesouder@edu.ece.fr>
2026-03-03 23:51:05 +0100

    New drivers-teams price

--- db/seeds/2026/driver_seasons.sql
+++ db/seeds/2026/driver_seasons.sql
@@ -2,35 +2,35 @@ INSERT INTO driver_seasons (driver_id, season, constructor_id, rookie, price) VA
 
 /* ========= INVICTA RACING ========= */
 (1,  2026, 1, 1, 24.0),  -- Rafael Câmara
-(2,  2026, 1, 0, 21.0),  -- Joshua Dürksen
+(2,  2026, 1, 0, 20.0),  -- Joshua Dürksen
 
 /* ========= HITECH TGR ========= */
 (3,  2026, 2, 0, 14.0),  -- Ritomo Miyata
-(4,  2026, 2, 1, 13.0),  -- Colton Herta
+(4,  2026, 2, 1, 15.0),  -- Colton Herta
 
 /* ========= CAMPOS RACING ========= */
-(5,  2026, 3, 1, 16.0),  -- Noel León
+(5,  2026, 3, 1, 15.0),  -- Noel León
 (6,  2026, 3, 1, 20.0),  -- Nikola Tsolov
 
 /* ========= DAMS LUCAS OIL ========= */
-(7,  2026, 4, 0, 21.0),  -- Dino Beganovic
+(7,  2026, 4, 0, 20.0),  -- Dino Beganovic
 (8,  2026, 4, 1, 14.0),  -- Roman Bilinski
 
 /* ========= MP MOTORSPORT ========= */
-(9,  2026, 5, 0, 17.0),  -- Gabriele Minì
-(10, 2026, 5, 0, 19.0),  -- Oliver Goethe
+(9,  2026, 5, 0, 15.0),  -- Gabriele Minì
+(10, 2026, 5, 0, 15.0),  -- Oliver Goethe
 
 /* ========= PREMA RACING ========= */
-(11, 2026, 6, 0, 14.0),  -- Sebastián Montoya
-(12, 2026, 6, 1, 18.0),  -- Mari Boya
+(11, 2026, 6, 0, 15.0),  -- Sebastián Montoya
+(12, 2026, 6, 1, 16.0),  -- Mari Boya
 
 /* ========= RODIN MOTORSPORT ========= */
-(13, 2026, 7, 1, 20.0),  -- Martinius Stenshorne
+(13, 2026, 7, 1, 18.0),  -- Martinius Stenshorne
 (14, 2026, 7, 0, 22.0),  -- Alexander Dunne
 
 /* ========= ART GRAND PRIX ========= */
-(15, 2026, 8, 0, 8.0),   -- Kush Maini
-(16, 2026, 8, 1, 10.0),  -- Tasanapol Inthraphuvasak
+(15, 2026, 8, 0, 9.0),   -- Kush Maini
+(16, 2026, 8, 1, 12.0),  -- Tasanapol Inthraphuvasak
 
 /* ========= AIX RACING ========= */
 (17, 2026, 9, 1, 7.0),   -- Emerson Fittipaldi Jr.
@@ -38,7 +38,7 @@ INSERT INTO driver_seasons (driver_id, season, constructor_id, rookie, price) VA
 
 /* ========= VAN AMERSFOORT RACING ========= */
 (19, 2026, 10, 1, 7.0),  -- Nicolás Varrone
-(20, 2026, 10, 1, 5.0),  -- Rafael Villagómez
+(20, 2026, 10, 1, 10.0),  -- Rafael Villagómez
 
 /* ========= TRIDENT ========= */
 (21, 2026, 11, 1, 10.0), -- Laurens van Hoepen
--- db/seeds/constructors.sql
+++ db/seeds/constructors.sql
@@ -1,13 +1,13 @@
 INSERT INTO constructors (name, slug, nationality, price) VALUES
-('Invicta Racing', 'invicta', 'United Kingdom', 25),
-('Hitech TGR', 'hitech', 'United Kingdom', 18),
-('Campos Racing', 'campos', 'Spain', 18),
-('DAMS Lucas Oil', 'dams', 'France', 16),
-('MP Motorsport', 'mp', 'Netherlands', 14),
-('PREMA Racing', 'prema', 'Italy', 15),
-('Rodin Motorsport', 'rodin', 'New Zealand', 18),
-('ART Grand Prix', 'art', 'France', 10),
-('AIX Racing', 'aix', 'United Arab Emirates', 8),
-('Van Amersfoort Racing', 'var', 'Netherlands', 8),
-('Trident', 'trident', 'Italy', 8)
+('Invicta Racing', 'invicta', 'United Kingdom', 22),
+('Hitech TGR', 'hitech', 'United Kingdom', 14.5),
+('Campos Racing', 'campos', 'Spain', 17.5),
+('DAMS Lucas Oil', 'dams', 'France', 17),
+('MP Motorsport', 'mp', 'Netherlands', 15),
+('PREMA Racing', 'prema', 'Italy', 15.5),
+('Rodin Motorsport', 'rodin', 'New Zealand', 20),
+('ART Grand Prix', 'art', 'France', 10.5),
+('AIX Racing', 'aix', 'United Arab Emirates', 6),
+('Van Amersfoort Racing', 'var', 'Netherlands', 8.5),
+('Trident', 'trident', 'Italy', 8.5)
 ON CONFLICT (slug) DO NOTHING;
